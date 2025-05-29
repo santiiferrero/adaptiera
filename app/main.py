8a9cf18c33eeba608bb91cbb90bb337cc43e3bdf
santi <santiagorferrero@gmail.com>
2025-05-29 15:20:33 -0300

    feat(files): correccion imports en main para streamlit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 import streamlit as st
-from app.views.chatbot import lanzar_chatbot
-from app.views.form_candidate_contact import mostrar_formulario as mostrar_formulario_candidatos
+import sys
+import os
+# Añadir el directorio actual al path para los imports
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from views.chatbot import lanzar_chatbot
+from views.form_candidate_contact import mostrar_formulario as mostrar_formulario_candidatos
 
 # Configuración de la página
 st.set_page_config(
